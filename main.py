30041c8b0924e8b85eee3d0fe8ee7b46239157d2
Josh <joshfokis@gmail.com>
2023-04-11 18:10:44 -0500

    fixed json files

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import tkinter as tk
 from tkinter import ttk
 
 def parse(file):
+    objs = []    
     if not file:
         return
     with open(file, "rb") as f:
@@ -14,49 +15,17 @@ def parse(file):
 def create_fields():
     pass
 
-
 def example_main():
-    # Create a new tkinter window
+   # Create a new tkinter window
     window = tk.Tk()
 
     # Create a section for personal information
     personal_info_frame = tk.Frame(window)
     personal_info_frame.pack(fill="both", expand=True)
-
-    name_label = tk.Label(personal_info_frame, text="Name:")
-    name_label.grid(row=0, column=0)
-
-    name_entry = tk.Entry(personal_info_frame)
-    name_entry.grid(row=0, column=1)
-
-    age_label = tk.Label(personal_info_frame, text="Age:")
-    age_label.grid(row=1, column=0)
-
-    age_entry = tk.Entry(personal_info_frame)
-    age_entry.grid(row=1, column=1)
-
-    # Create a section for address information
-    address_frame = tk.Frame(window)
-    address_frame.pack(fill="both", expand=True)
-
-    street_label = tk.Label(address_frame, text="Street:")
-    street_label.grid(row=0, column=0)
-
-    street_entry = tk.Entry(address_frame)
-    street_entry.grid(row=0, column=1)
-
-    city_label = tk.Label(address_frame, text="City:")
-    city_label.grid(row=1, column=0)
-
-    city_entry = tk.Entry(address_frame)
-    city_entry.grid(row=1, column=1)
-
-    state_label = tk.Label(address_frame, text="State:")
-    state_label.grid(row=2, column=0)
-
-    state_entry = tk.Entry(address_frame)
-    state_entry.grid(row=2, column=1)
-
+    
+    for k,v in a.items():
+        tk.Label(personal_info_frame, text=k)
+        tk.Entry(personal_info_frame, text=v)
     # Start the main event loop to display the window
     window.mainloop()
 
