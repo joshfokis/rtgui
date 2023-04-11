a1c8b110c063080fbe153ef44216728aa18daf26
Psuedoo <forthewin720@gmail.com>
2023-04-11 19:31:09 -0400

    create tk tree

--- main.py
+++ main.py
@@ -1,18 +1,21 @@
 import bson
 import json
 import tkinter as tk
+from tkinter import ttk
 
 def parse(file):
     if not file:
         return
     with open(file, "rb") as f:
-        for obj in bson.decode_file_iter(f):
+        data = json.load(f)
+        for obj in data:
             yield obj
 
 def create_fields():
     pass
 
-def main():
+
+def example_main():
     # Create a new tkinter window
     window = tk.Tk()
 
@@ -57,14 +60,32 @@ def main():
     # Start the main event loop to display the window
     window.mainloop()
 
+def main():
+    queues = parse("rtq.json")
+    custom_fields = parse("rt_customfields.json")
+
+    root = tk.Tk()
+    root.title("RTQ")
+    root.geometry("800x600")
+
+    
+    q_widget = ttk.Treeview(root)
+    q_widget.pack(fill="both", expand=True)
+
+    # queue_widget = tk.Listbox(root)
+    # queue_widget.pack(fill="both", expand=True)
+
+    for queue in queues:
+        id = q_widget.insert('', 0, text = f"ID: {queue['id']}")
+        q_widget.insert(id, "end", text = f"Name: {queue['Name']}")
+        q_widget.insert(id, "end", text = f"Created: {queue['Created']}")
+        q_widget.insert(id, "end", text = f"Last Updated: {queue['LastUpdated']}")
+        # q_widget.insert(id, "end", text = f"Name: {queue['Name']}")
+
+
+
+
+    root.mainloop()
+
 if __name__ == "__main__":
     main()
-
-#def main():
-#    queues = parse("rtq.bson")
-#    custom_fields = parse("custom_fields.bson")
-#    root = tk.Tk()
-#    root.title("RTQ")
-#    root.geometry("800x600")
-#    root.mainloop()
-#    pass
